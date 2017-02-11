5b35b188d82829a468c7f4618f563d074f8c01a9
Dell Laptop <Dell Laptop>
2017-02-11 14:14:31 +0545

    Dialog box added

---
+++ src/components/dialog/index.js
@@ -0,0 +1,18 @@
+import styles from '../../stylesheets/index.scss'
+
+const DialogBox = (props) => {
+  return(
+  <div className={styles.dialog}>
+    <div className={styles.dialog_content}>
+      <div className={styles.dialog_title}>
+        Login
+      </div>
+      <div className={styles.dialog_close}>
+        Close
+      </div>
+      props.content
+    </div>
+  </div>
+  )
+}
+export default DialogBox
